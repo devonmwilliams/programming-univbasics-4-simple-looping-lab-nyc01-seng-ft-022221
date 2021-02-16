6f8f1d58821d671e94d1e98de77ff5a03fdb7aa5
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-16 03:27:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,3 +2,9 @@ def loop_message_five_times(message)
   message = "Hello World."
   5.times {puts message}
 end
+
+def loop_message_n_times(message, 5)
+  message = "Hello World."
+  5.times {puts message}
+end
+  
